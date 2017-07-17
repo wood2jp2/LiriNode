445ac76a9631334cfba01bbdb8497fc95edf28be
Josh Wood <wood2jp2@gmail.com>
2017-07-17 17:08:42 -0400

    files set up, twitter keys noted

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Y0e5wdHnL6UoOQFZDTwD6P55E',
+  consumer_secret: 'jbSc6bNnsUnswwXfCta8bgW4qwMPaKLt4lcD7UkdvKe5Chdlhi',
+  access_token_key: '304206399-ecKvf7gWgOp1Zp2kbtzriYZ7KOymDCv6UqROXYDL',
+  access_token_secret: 'z3LWMISJps7pcTKuSTwJWBo8XmiCtuHEgsZhYxjgdpHUX',
+}
