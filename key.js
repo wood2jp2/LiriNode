ee18beb383d1ae180706387d4dd67b391c09b25b
Josh Wood <wood2jp2@gmail.com>
2017-07-18 23:39:58 -0400

    objectified the code...Methodology. Spotify pulling down data

--- key.js
+++ key.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '304206399-ecKvf7gWgOp1Zp2kbtzriYZ7KOymDCv6UqROXYDL',
   access_token_secret: 'z3LWMISJps7pcTKuSTwJWBo8XmiCtuHEgsZhYxjgdpHUX',
 }
+
+exports.spotifyKeys = {
+  id: 'cc249374f82940bbb1fd70f232777215',
+  secret: 'cc9fcf29f6e14c3aae78735326b1dc58',
+}
--- liri.js
+++ liri.js
@@ -1,26 +1,52 @@
-var Twitter = require('twitter');
-var twitterKeys = require('./key').twitterKeys;
+var nodinIt = {
 
-var client = new Twitter(twitterKeys);
+  userCommand: process.argv[2],
+  userSongChoice: process.argv[3],
 
-var params = {
-  screen_name: 'WhatWoodJDubDo',
-  count: 20
-};
+  getTwitter: function() {
+    var Twitter = require('twitter');
+    var twitterKeys = require('./key').twitterKeys;
+    var client = new Twitter(twitterKeys);
+    var params = {
+      screen_name: 'WhatWoodJDubDo',
+      count: 20
+    };
 
-var userInput = process.argv[2];
-
-if (userInput === "my-tweets") {
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error && response.statusCode === 200) {
-      // console.log(tweets);
-      // console.log(response);
-      // console.
-      for (var i = 0; i < 20; i++) {
-        console.log(tweets[i].text);
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error && response.statusCode === 200) {
+        for (var i = 0; i < 20; i++) {
+          console.log(tweets[i].text);
+        }
+      } else {
+        console.log(error);
       }
-    } else {
-      console.log(error);
-    }
-  })
+    });
+  },
+
+  getSpotify: function() {
+    var Spotify = require('node-spotify-api');
+    var spotifyKeys = require('./key').spotifyKeys;
+    var spotify = new Spotify(spotifyKeys);
+
+    spotify.search({
+      type: 'track',
+      query: this.userSongChoice || 'The Sign'
+    }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      console.log(data.tracks.items[0])
+    });
+  },
 };
+
+if (nodinIt.userCommand === "my-tweets") {
+  nodinIt.getTwitter();
+} else if (nodinIt.userCommand === 'spotify-this-song') {
+  if (nodinIt.userSongChoice === '') {
+    nodinIt.userSongChoice === 'The Sign';
+  } else {
+    nodinIt.getSpotify();
+  }
+}
