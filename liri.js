b114311369658c380b6a3d4067392f6a583f061a
Josh Wood <wood2jp2@gmail.com>
2017-07-19 22:12:31 -0400

    pulling all omdb data

--- liri.js
+++ liri.js
@@ -1,12 +1,16 @@
 // Object
 var nodinIt = {
-  // global variables,
+  // global variables, continuations of node array
   userCommand: process.argv[2],
   userSongChoice: process.argv[3] || 'The Sign',
+  userMovie: process.argv[3] || 'Mr. Nobody',
 
   getTwitter: function() {
+    // node knows to look for Twitter in node_modules folder
     var Twitter = require('twitter');
+    // calling keys in key.js and assigning them
     var twitterKeys = require('./key').twitterKeys;
+    // npm syntax for twitter package
     var client = new Twitter(twitterKeys);
     var params = {
       screen_name: 'WhatWoodJDubDo',
@@ -14,7 +18,9 @@ var nodinIt = {
     };
 
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      // if data retrieval is success...
       if (!error && response.statusCode === 200) {
+        // counting thru and printing tweets
         for (var i = 0; i < 20; i++) {
           console.log(tweets[i].text);
         }
@@ -25,6 +31,7 @@ var nodinIt = {
   },
 
   getSpotify: function() {
+    // same variables / syntax essentially as above
     var Spotify = require('node-spotify-api');
     var spotifyKeys = require('./key').spotifyKeys;
     var spotify = new Spotify(spotifyKeys);
@@ -38,10 +45,31 @@ var nodinIt = {
       }
       // Console log artist, song name, preview link (external URL), song album
       console.log("Artist: " + data.tracks.items[0].artists[0].name);
-      // ASK VINNY why i can't simply log '(this.userSongChoice)', even though we are calling function outside of object
+      // ASK VINNY why i can't simply log '(this.userSongChoice)', even though we are calling function outside of object (still accessing object)
       console.log(nodinIt.userSongChoice);
       console.log("Album: " + data.tracks.items[0].album.name);
       console.log("Preview Link: " + data.tracks.items[0].external_urls.spotify);
+    });
+  },
+
+  getOMDB: function() {
+    var request = require('request');
+    var omdbSite = "http://www.omdbapi.com/?t=" + this.userMovie + "&y=&plot=short&apikey=40e9cece";
+
+    // title, year, IMDB rating, Rotten Tomatoes rating, country, language, Plot, Actors
+    request(omdbSite, function(error, response, body) {
+      body = JSON.parse(body);
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+      console.log('Movie Title: ' + body.Title);
+      console.log('Year: ' + body.Year);
+      console.log('IMDB Rating: ' + body.imdbRating);
+      console.log('Rotten Tomatoes: ' + body.Ratings[1].Value);
+      console.log('Country: ' + body.Country);
+      console.log('Language: ' + body.Language);
+      console.log('Plot: ' + body.Plot);
+      console.log('Actors: ' + body.Actors);
     });
   },
 };
@@ -50,4 +78,6 @@ if (nodinIt.userCommand === "my-tweets") {
   nodinIt.getTwitter();
 } else if (nodinIt.userCommand === 'spotify-this-song') {
   nodinIt.getSpotify();
+} else if (nodinIt.userCommand === 'movie-this') {
+  nodinIt.getOMDB();
 }
